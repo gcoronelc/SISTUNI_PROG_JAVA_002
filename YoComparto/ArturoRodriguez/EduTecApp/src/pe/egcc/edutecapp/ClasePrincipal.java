4103cd49dadacdec7f87e6f0054d0b1dff2f7040
Gustavo Coronel <gcoronelc@gmail.com>
2016-02-07 13:51:16 -0500

    Clase 02

---
+++ YoComparto/ArturoRodriguez/EduTecApp/src/pe/egcc/edutecapp/ClasePrincipal.java
@@ -0,0 +1,12 @@
+
+package pe.egcc.edutecapp;
+
+import pe.egcc.edutecapp.view.Appview;
+
+public class ClasePrincipal {
+
+    public static void main(String[] args) {
+        Appview.main(null);
+    }
+    
+}
